c764598453aba41b6ae8f18f5299aaf7bccb0b69
muhammadshiva <shivamuhammad07@gmail.com>
2021-06-14 22:02:33 +0700

    feat(controller): Integrate guru

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -2,17 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mapel;
+use App\Models\Tugas;
 use Illuminate\Http\Request;
 
 class GuruController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('guru');
+        $mapels = Mapel::orderBy('id', 'asc')->paginate(5);
+        return view('pages.guru.home', compact('mapels'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tugas = Tugas::where('id_mapel', $id)
+            ->orderBy('id_mapel', 'asc')
+            ->get();
+        return view('pages.guru.tugas', compact('tugas'));
+        // return dd($tugas);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
